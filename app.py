1dcccc7a60f99d646e76f26422c56bbd4497d53a
Mafaz Manzil <90832527+Mafaz5128@users.noreply.github.com>
2025-01-16 11:49:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ st.markdown(
 )
 st.link_button(
     label="Data set Maker",
-    url="https://datasetmaker-3uy2exn2eq89ubuazxwwwu.streamlit.app/"
+    url="https://datasetmaker-sla-dashboard.streamlit.app/"
 )
 # File uploader widget
 uploaded_file = st.file_uploader("Upload your Excel file", type=["csv"])
