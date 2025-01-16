e2db0151d7bd22cdea02980f002e469e039e5776
Mafaz Manzil <90832527+Mafaz5128@users.noreply.github.com>
2025-01-16 12:54:07 +0530

    Update datasetMaker.py

--- datasetMaker.py
+++ datasetMaker.py
@@ -3,8 +3,7 @@ import streamlit as st
 
 # Function to process each uploaded file
 def process_excel(file, columns_reference=None, month=None):
-    df = pd.read_excel(file, header=[0, 1, 2, 3, 4])
-    df = df.iloc[:, 18:]   
+    df = pd.read_excel(file, header=[0, 1, 2, 3, 4])  
     # Combine multi-level header into a single-level header
     df.columns = ['_'.join([str(col).strip() for col in col_tuple]) for col_tuple in df.columns.values]
 
@@ -15,6 +14,9 @@ def process_excel(file, columns_reference=None, month=None):
     # Add a 'Month' column and fill it with the selected month
     if month is not None:
         df['Month'] = month
+
+    # Select only the first 18 columns
+    df = df.iloc[:, :18]
 
     return df
 
